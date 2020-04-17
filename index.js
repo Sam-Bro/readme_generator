bb3c3bd5cd56aeab49467083761053b98a0d6e0d
OrbmasWin <sambrostudent@gmail.com>
2020-04-17 15:00:05 +0800

    added basic readme questions and function

---
+++ index.js
@@ -0,0 +1,80 @@
+var fs = require ('fs');
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: "projectName",
+        message: "What is the name of your project?"
+    },
+    {
+        type: 'input',
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: 'input',
+        name: "installation",
+        message: "What is needed to get your project running?"
+    }
+  ])
+  .then(({name, projectName, installation }) => {
+    const readMeGenHTML = `
+# ${projectName}
+
+made by ${name}
+
+### {{short description}} 
+
+{{description}}
+
+### Installation
+
+To install and use this application, ${installation}
+    `;
+console.log(readMeGenHTML);
+fs.writeFile("README.md", readMeGenHTML, (err) => {
+    if (err) console.error(err);
+    else console.log("File Written!");
+    });
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+
+
+
+
+
+  /*
+  ,
+    {
+        type: 'input',
+        name: "usage",
+        message: "How do you use the project?"
+    },
+    {
+        type: 'input',
+        name: "licence",
+        message: "What licenses does your project have?"
+    },
+    {
+        type: 'input',
+        name: "contributing",
+        message: "how are people able to contribute?"
+    },
+    {
+        type: 'input',
+        name: "tests",
+        message: "what tests?"
+    },
+    {
+        type: 'input',
+        name: "questions",
+        message: "questions?"
+    }
+    */
