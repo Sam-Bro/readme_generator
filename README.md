d1fb978b2e39e8c5d0e67f8e4f7816caeaf7185e
OrbmasWin <sambrostudent@gmail.com>
2020-04-19 16:10:12 +0800

    retry test commit

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 
 # README generator
-![version](https://img.shields.io/badge/version-a1.6-blue)
+![version](https://img.shields.io/badge/version-a 1.6.1-blue)
 
 made by Sam
 
 > dynamically generate a README from a users input 
 
-{{description}}
+a command-line application that dynamically generates a README.md from a user's input, the application will be invoked with the command "node index.js". The user will be prompted for their name, project description and other information pertaining to the project the README is for.
+
+## Table of Contents
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Licence](#Licence)
+- [Contributing](#Contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
 
 ### Installation
-To install and use this application, To install and use this application, use NPM to install: Inquirer, fs and gh badges
+To install and use this application, To install this application, use NPM to install: Inquirer, fs and gh-badges
+
+## usage
+To use the application, open terminal in the folder containing the index.js file. enter "node index.js" into the terminal and answer the prompts, once all of the prompts have been answered a README.md file will be created in the same file as index.js.
+
+## licence
+No licences 
+
+## contributing
+contributions are not currently open to public.
+Contact repository owner with any inquiries regarding contributions.
     
--- index.js
+++ index.js
@@ -24,10 +24,10 @@ inquirer
       message: "What is needed to get your project running?",
     },
     {
-      type: "input", 
+      type: "input",
       name: "versionNo",
-      message: "What is the version number of your project?"
-    }
+      message: "What is the version number of your project?",
+    },
   ])
   .then(({ name, projectName, shortDescription, installation, versionNo }) => {
     const readMeGenHTML = `
@@ -38,10 +38,30 @@ made by ${name}
 
 > ${shortDescription} 
 
-{{description}}
+a command-line application that dynamically generates a README.md from a user's input, the application will be invoked with the command "node index.js". The user will be prompted for their name, project description and other information pertaining to the project the README is for.
+
+## Table of Contents
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Licence](#Licence)
+- [Contributing](#Contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
 
 ### Installation
 To install and use this application, ${installation}
+
+## usage
+To use the application, open terminal in the folder containing the index.js file. enter "node index.js" into the terminal and answer the prompts, once all of the prompts have been answered a README.md file will be created in the same file as index.js.
+
+## licence
+No licences 
+
+## contributing
+contributions are not currently open to public.
+Contact repository owner with any inquiries regarding contributions.
     
 `;
 
